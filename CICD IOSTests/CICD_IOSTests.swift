d7c65949b8ab4f83783bdc3e547e149464531f0c
Awaisabid332 <Awaisabid332@gmail.com>
2022-08-09 18:44:33 +0500

    test added

---
+++ CICD IOSTests/CICD_IOSTests.swift
@@ -0,0 +1,24 @@
+//
+//  CICD_IOSTests.swift
+//  CICD IOSTests
+//
+//  Created by AWS Macbook on 09/08/2022.
+//
+
+import XCTest
+@testable import CICD_IOS
+
+class CICD_IOSTests: XCTestCase {
+    
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        
+    }
+}
